1a99848c2e4c525c42438bb16ab28a20dd17e226
Keith Petrone <keithpetrone@outlook.com>
2020-03-24 22:28:48 -0600

    Added invite tracking.

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ textByLine = text.toString().split("\n");
 mixers = textByLine;
 console.log("Mixer user list has been loaded...");
 
+// Initialize the invite cache
+const invites = {};
+
 //Logging into Discord
 client.on('ready', () => {
     console.log(`Discord: Logged in as ${client.user.tag}!`);
@@ -67,6 +70,28 @@ client.on('ready', () => {
 client.on('guildMemberAdd', (member) => {
     console.log(member.user.tag.toString() + " has joined the server!");
     member.send(config.discord.welcomeMessage);
+
+    // To compare, we need to load the current invite list.
+  member.guild.fetchInvites().then(guildInvites => {
+    // This is the *existing* invites for the guild.
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = client.users.get(invite.inviter.id);
+    // Get the log channel (change to your liking)
+    const logChannel = member.guild.channels.find(channel => channel.name === "logging");
+    // A real basic message with the information we need. 
+    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+});
+
+// Load all invites for all guilds and save them to the cache.
+client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+        invites[g.id] = guildInvites;
+    });
 });
 
 client.on('message', msg => {
